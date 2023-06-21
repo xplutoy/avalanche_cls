34524394b9620ba4bc9b67db473eb8b936c6a7b8
yx <yangxue.cs@foxmail.com>
2023-06-21 09:29:53 +0800

    online_er

---
+++ cifar100/native_online_er.py
@@ -0,0 +1,89 @@
+from avalanche.benchmarks import SplitCIFAR100
+from avalanche.benchmarks.generators import benchmark_with_validation_stream
+from avalanche.benchmarks.scenarios.online_scenario import OnlineCLScenario
+from avalanche.evaluation.metrics import (accuracy_metrics, forgetting_metrics,
+                                          loss_metrics)
+from avalanche.logging import InteractiveLogger
+from avalanche.models import pytorchcv_wrapper
+from avalanche.training.plugins import EvaluationPlugin, ReplayPlugin
+from avalanche.training.storage_policy import ReservoirSamplingBuffer
+from avalanche.training.supervised import OnlineNaive
+from torch.nn import CrossEntropyLoss
+from torch.optim import SGD
+
+from utils import get_device, set_seed
+
+
+def native_online_er_cifar100(args):
+    set_seed(args.seed)
+    device = get_device(args.cuda)
+
+    benchmark = SplitCIFAR100(
+        args.task_num,
+        return_task_id=False,
+        seed=args.seed,
+        fixed_class_order=list(range(100)),
+        shuffle=True,
+        class_ids_from_zero_in_each_exp=False,
+    )
+
+    benchmark = benchmark_with_validation_stream(benchmark, 0.05)
+
+    model = pytorchcv_wrapper.resnet("cifar100", depth=20, pretrained=False)
+
+    eval_plugin = EvaluationPlugin(
+        accuracy_metrics(minibatch=False, epoch=True, experience=True, stream=True),
+        loss_metrics(minibatch=True, epoch=True, experience=True, stream=True),
+        forgetting_metrics(experience=True, stream=True),
+        loggers=[InteractiveLogger()],
+        strict_checks=False,
+    )
+
+    replay_plugin = ReplayPlugin(
+        mem_size=args.mem_size,
+        batch_size_mem=args.batch_size_mem,
+        storage_policy=ReservoirSamplingBuffer(args.mem_size)
+    )
+
+    optimizer = SGD(model.parameters(), lr=args.lr, momentum=0.9)
+    criterion = CrossEntropyLoss()
+
+    cl_strategy = OnlineNaive(
+        model,
+        optimizer,
+        criterion,
+        train_passes=1,
+        train_mb_size=args.train_mb_size,
+        eval_mb_size=args.eval_mb_size,
+        device=device,
+        plugins=[replay_plugin],
+        evaluator=eval_plugin,
+    )
+
+    batch_streams = benchmark.streams.values()
+
+    for t, experience in enumerate(benchmark.train_stream):
+        print("Start of experience: ", experience.current_experience)
+        print("Current Classes: ", experience.classes_in_this_experience)
+
+        ocl_scenario = OnlineCLScenario(
+            original_streams=batch_streams,
+            experiences=experience,
+            experience_size=args.train_mb_size,
+            access_task_boundaries=False,
+        )
+
+        cl_strategy.classes_in_this_experience = experience.classes_in_this_experience
+
+        cl_strategy.train(
+            ocl_scenario.train_stream,
+            eval_streams=[])
+        cl_strategy.eval(benchmark.test_stream[:t+1])
+
+    print("Computing accuracy on the whole test set")
+    cl_strategy.eval(benchmark.test_stream)
+
+
+
+
+
--- runs.py
+++ runs.py
@@ -1,4 +1,4 @@
-from cifar100 import native_er
+from cifar100 import native_er, native_online_er
 from utils import create_default_args
 
 native_er_cifar100_args = create_default_args(
@@ -14,4 +14,19 @@ native_er_cifar100_args = create_default_args(
         "batch_size_mem": 10,
     })
 
-native_er.native_er_cifar100(native_er_cifar100_args)
+# native_er.native_er_cifar100(native_er_cifar100_args)
+
+native_online_er_cifar100_args = create_default_args(
+    {
+        "cuda": 0,
+        "task_num": 20,
+        "mem_size": 2000,
+        "lr": 0.1,
+        "train_mb_size": 10,
+        "eval_mb_size": 500,
+        "seed": 123,
+        "batch_size_mem": 10,
+    })
+
+
+native_online_er.native_online_er_cifar100(native_online_er_cifar100_args)
